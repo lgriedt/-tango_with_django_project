2ee925335e58a3693fb528e107a355c6eea2c084
Tareq Lgried <2619592L@student.gla.ac.uk>
2024-02-05 19:12:58 +0000

    chapter 3 and 4 after tests

---
+++ rango/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from rango import views
+
+app_name = 'rango'
+
+urlpatterns = [
+    path('index/', views.index, name='index'),
+    path('about/', views.about, name='about'),
+]
