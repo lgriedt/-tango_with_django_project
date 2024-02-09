f2fe841d00500ad7a9fb481f849d78a954228a12
Tareq Lgried <2619592L@student.gla.ac.uk>
2024-02-09 12:24:26 +0000

    chp8 done

--- rango/views.py
+++ rango/views.py
@@ -20,8 +20,10 @@ def index(request):
 
 
 def about(request):
-    return render(request, 'rango/about.html')
-
+    print(request.method)
+    print(request.user)
+    return render(request, 'rango/about.html', {})
+    
 
 def show_category(request, category_name_slug):
     context_dict = {}
